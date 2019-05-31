2063be7f113e80fc1ac6b1e260a947aaa80a61d8
Misael Augusto <misa2546@gmail.com>
2019-05-31 09:51:22 -0300

    Atualizando de tela de informações

--- src/pages/Dados/index.js
+++ src/pages/Dados/index.js
@@ -5,15 +5,28 @@ import styles from './styles';
 
 class Dados extends Component {
   state = {
-    aluno: []
+    aluno: [
+      {
+        id: 1,
+        name: 'José Anderson',
+        data_de_nascimento: '12-05-2007',
+        rg: 875.634,
+        sexo: 'Masculino',
+        cpf: '897.265.954-21',
+        turma: '8° ano',
+        turno: 'tarde',
+        mae: 'Ana Margarida',
+        pai: 'João Paulo'
+      }
+    ]
   }
 
-  async componentDidMount() {
-    const response = await api.get('/1');
+  // async componentDidMount() {
+  //   const response = await api.get('/1');
 
-    console.log(response);
-    this.setState({ aluno: response.data });
-  }
+  //   console.log(response);
+  //   this.setState({ aluno: response.data });
+  // }
 
   render() {
     // console.log(this.state.aluno)
@@ -22,11 +35,15 @@ class Dados extends Component {
          <ScrollView>
          { this.state.aluno.map(aluno => (
           <View key={aluno.id} style={styles.containerDados}>
-              <Text style={styles.name}>{aluno.nome}</Text>
+              <Text style={styles.name}>{aluno.name}</Text>
               <Text style={styles.data}>Dt_nascimento: {aluno.data_de_nascimento}</Text>
               <Text style={styles.rg}>RG: {aluno.rg}</Text>
               <Text style={styles.email}>Sexo: {aluno.sexo}</Text>
               <Text style={styles.bairro}>CPF: {aluno.cpf}</Text>
+              <Text style={styles.bairro}>Turma: {aluno.turma}</Text>
+              <Text style={styles.bairro}>Turno: {aluno.turno}</Text>
+              <Text style={styles.bairro}>Mãe: {aluno.mae}</Text>
+              <Text style={styles.bairro}>Pai: {aluno.pai}</Text>
           </View>
         )) }
         </ScrollView>
--- src/pages/Dados/styles.js
+++ src/pages/Dados/styles.js
@@ -7,33 +7,39 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     containerDados: {
-        backgroundColor: "#ff8c00",
-        padding: 20,
-        marginTop: 20,
-        justifyContent: "center",
-        alignItems: "center",
+        padding: 30,
+        marginTop: 40,
+        backgroundColor: '#ff8c00',
+        borderRadius: 4
     },
     name: {
         fontWeight: 'bold',
         fontSize: 20,
         color: "#111",
         textTransform: 'uppercase',
+        flex: 1,
+        textAlign: 'center',
+        marginBottom: 20,
     },
     data: {
         fontWeight: 'bold',
-        color: "#111"
+        color: "#111",
+        fontSize: 17
     },
     rg: {
         fontWeight: 'bold',
-        color: "#111"
+        color: "#111",
+        fontSize: 17
     },
     email: {
         fontWeight: 'bold',
-        color: "#111"
+        color: "#111",
+        fontSize: 17
     },
     bairro: {
         fontWeight: 'bold',
-        color: "#111"
+        color: "#111",
+        fontSize: 17
     }
 });
 
